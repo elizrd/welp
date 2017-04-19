fcb75cd2faae4a1e21c3e09107e4911cdb411a7e
Elizabeth Connors <poodle.fury@gmail.com>
2017-04-19 21:03:42 +0000

    Made rating dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+  
+  RATINGS = {
+    'one shrug'     => '1_shrug',
+    'two shrugs'    => '2_shrugs',
+    'three shrugs'  => '3_shrugs',
+    'four shrugs'   => '4_shrugs',
+    'five shrugs'   => '5_shrugs'
+  }
+  
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
