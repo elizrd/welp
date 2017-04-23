82dcaee0025409c8491305b7fc35efe2097d4e57
Elizabeth Connors <poodle.fury@gmail.com>
2017-04-23 17:28:07 +0000

    created user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-devise_for :users
+  devise_for :users
 
-root 'places#index'
+  root 'places#index'
 
-resources :places do
-  resources :comments, only: :create
-  resources :photos, only: :create
-end
-#Priority is based upon order of creating: first created -> highest priority
+  resources :places do
+    resources :comments, only: :create
+    resources :photos, only: :create
+  end
+  #Priority is based upon order of creating: first created -> highest priority
+
+  resources :users, only: :show
 
 end
